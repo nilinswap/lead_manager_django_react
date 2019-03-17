adadca60cf7ec15319af1b044c9b4be7edfc31a2
nilinswap <nilinswap@gmail.com>
2019-03-17 23:11:40 +0530

    set up for redux

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 
 import Header from './layout/Header';
 import Dashboard from './leads/Dashboard';
+import { Provider } from 'react-redux';
+import store from '../store';
 
 class App extends Component{
     render(){
